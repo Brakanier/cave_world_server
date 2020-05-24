526c56853f47461d556fea5d83654a63fae05a3b
Brakanier <18brakanier.forest@gmail.com>
2020-05-24 18:47:15 +0300

    fix

--- services/game.py
+++ services/game.py
@@ -81,8 +81,8 @@ class Game:
             if enemy_connect:
                 enemy_user = enemy_connect.user
             else:
-                enemy = await UserData.filter(user__id=data["id"]).prefetch_related('user').get_or_none()
-                enemy_user = enemy.user if enemy else None
+                enemy_user = await User.filter(id=data["id"]).prefetch_related('data').get_or_none()
+
             if not enemy_user:
                 return
             
@@ -107,7 +107,6 @@ class Game:
 
     @atomic()
     async def attack(self, user: User, enemy: User):
-        await enemy.data.get_or_none()
         await enemy.data.processing()
         battle = await self.war.attack(user, enemy)
         if battle:
